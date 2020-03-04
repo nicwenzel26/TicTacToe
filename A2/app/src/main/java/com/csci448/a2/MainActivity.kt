c2e1f8b70ea6d651c4e29b74cec9707cbb1969d6
nwenzel <nwenzel@mymail.mines.edu>
2020-03-03 17:22:25 -0700

    Working initial homescreen

--- A2/app/src/main/java/com/csci448/a2/MainActivity.kt
+++ A2/app/src/main/java/com/csci448/a2/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if(currentFragment == null) {
+            val fragment = HomeScreenFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+        }
     }
 }
