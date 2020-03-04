b6d779cbc7d38b7104418aa3beb5bceabf41a3e6
nwenzel <nwenzel@mymail.mines.edu>
2020-03-04 09:00:10 -0700

    Added ability to click on each space

--- A2/app/src/main/java/com/csci448/a2/GameScreenFragment.kt
+++ A2/app/src/main/java/com/csci448/a2/GameScreenFragment.kt
@@ -4,9 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class GameScreenFragment:Fragment() {
+
+    private lateinit var button_11 : ImageButton
+    private lateinit var button_12 : ImageButton
+    private lateinit var button_13 : ImageButton
+    private lateinit var button_21 : ImageButton
+    private lateinit var button_22 : ImageButton
+    private lateinit var button_23 : ImageButton
+    private lateinit var button_31 : ImageButton
+    private lateinit var button_32 : ImageButton
+    private lateinit var button_33 : ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +28,58 @@ class GameScreenFragment:Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.game_screen_fragment, container, false)
 
+        initButtons(view)
+
+
+        val grid_11 = GridSpace(button_11)
+        val grid_12 = GridSpace(button_12)
+        val grid_13 = GridSpace(button_13)
+        val grid_21 = GridSpace(button_21)
+        val grid_22 = GridSpace(button_22)
+        val grid_23 = GridSpace(button_23)
+        val grid_31 = GridSpace(button_31)
+        val grid_32 = GridSpace(button_32)
+        val grid_33 = GridSpace(button_33)
+
+
+
+
+
+        button_11.setOnClickListener { checkSpace(grid_11) }
+        button_12.setOnClickListener { checkSpace(grid_12) }
+        button_13.setOnClickListener { checkSpace(grid_13) }
+        button_21.setOnClickListener { checkSpace(grid_21) }
+        button_22.setOnClickListener { checkSpace(grid_22) }
+        button_23.setOnClickListener { checkSpace(grid_23) }
+        button_31.setOnClickListener { checkSpace(grid_31) }
+        button_32.setOnClickListener { checkSpace(grid_32) }
+        button_33.setOnClickListener { checkSpace(grid_33) }
+
+
         return view
+    }
+    //Check if space is used. If yes tell the user, if no assign their piece
+    private fun checkSpace(gridSpace: GridSpace) {
+        if(gridSpace.xOrO == null) {
+            gridSpace.button.setImageResource(R.drawable.x)
+            gridSpace.xOrO = 'x'
+        }
+
+        else {
+            Toast.makeText(context, "That space is already taken", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    //Init buttons for all the grid spots
+    private fun initButtons(view: View) {
+        button_11 = view.findViewById(R.id.button_11)
+        button_12 = view.findViewById(R.id.button_12)
+        button_13 = view.findViewById(R.id.button_13)
+        button_21 = view.findViewById(R.id.button_21)
+        button_22 = view.findViewById(R.id.button_22)
+        button_23 = view.findViewById(R.id.button_23)
+        button_31 = view.findViewById(R.id.button_31)
+        button_32 = view.findViewById(R.id.button_32)
+        button_33 = view.findViewById(R.id.button_33)
     }
 }
---
+++ A2/app/src/main/java/com/csci448/a2/GridSpace.kt
@@ -0,0 +1,6 @@
+package com.csci448.a2
+
+import android.widget.ImageButton
+
+data class GridSpace(val button: ImageButton, var xOrO :Char? = null) {
+}
