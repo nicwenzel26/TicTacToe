15a4739e47a3fdbc1e7e19dd648c2a4efd679bfc
nwenzel <nwenzel@mymail.mines.edu>
2020-03-03 18:26:30 -0700

    Added menu items with icons to home screen

--- A2/app/src/main/java/com/csci448/a2/HomeScreenFragment.kt
+++ A2/app/src/main/java/com/csci448/a2/HomeScreenFragment.kt
@@ -1,12 +1,14 @@
 package com.csci448.a2
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 
 class HomeScreenFragment:Fragment() {
+
+    interface CallBacks {
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +17,17 @@ class HomeScreenFragment:Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.home_screen_fragment,container,false)
         return view
+    }
+
+    //TODO get icons and text for icons in menu bar
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.home_screen_fragment, menu)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
 }
