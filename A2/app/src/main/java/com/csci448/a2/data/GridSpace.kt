2286d07dac33780791c2ef15424fcb3b0016df8d
nwenzel <nwenzel@mymail.mines.edu>
2020-03-06 09:55:32 -0700

    Added comments to GridSpace

--- A2/app/src/main/java/com/csci448/a2/data/GridSpace.kt
+++ A2/app/src/main/java/com/csci448/a2/data/GridSpace.kt
@@ -3,5 +3,9 @@ package com.csci448.a2.data
 import android.widget.ImageButton
 import java.text.FieldPosition
 
-data class GridSpace(val button: ImageButton, var xOrO :Char? = null, val position: Int) {
-}
+/*
+Data class to hold the information for each avalible space on a Tic Tac Toe Board
+Holds the Image Button associated with the space, the current value (x or o) initially set to null
+and gives the position of the grid so it can be remade from a saved instance state.
+ */
+data class GridSpace(val button: ImageButton, var xOrO :Char? = null, val position: Int)
